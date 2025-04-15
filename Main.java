2ebc99821b3b5260aef3f578a6c25dec815d04c1
divyajai12 <d.jaigopal.045@studms.ug.edu.pl>
2025-04-15 16:00:22 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,23 +1,13 @@
-pipeline {
-    agent any
-    stages {
-        stage('Build') {
-            steps {
-                echo 'Building..'
-                // Here you can find the commands that build your application, such as mvn clean install
-            }
-        }
-        stage('Test') {
-            steps {
-                echo 'Testing..'
-                // Commands that run tests, such as mvn test
-            }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'Deploying..'
-                // Application deployment scripts
-            }
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello");
+
+        for (int i = 1; i <= 15; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
         }
     }
 }
